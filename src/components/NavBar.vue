ab59953591d85bb4a332ce02939ee4ae604c6e0f
Antonis <agiallelis1@gmail.com>
2021-03-28 17:08:43 +0300

    navbar update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,23 @@
 <template>
   <div>
     <b-navbar toggleable="lg" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">shopGo</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
 
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-navbar-nav>
+            <b-nav-item href="#">Sign Up</b-nav-item>
+          </b-navbar-nav>
+          
+          <b-navbar-nav>
+            <b-nav-item href="#">Sign In</b-nav-item>
+          </b-navbar-nav>
+          
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -51,17 +36,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   h3 {
-    margin: 40px 0 0;
-  }
+      margin: 40px 0 0;
+    }
+
   ul {
-    list-style-type: none;
-    padding: 0;
-  }
+      list-style-type: none;
+      padding: 0;
+    }
+
   li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+      display: inline-block;
+      margin: 0 10px;
+    }
+
   a {
-    color: #42b983;
-  }
+      color: #42b983;
+    }
 </style>
