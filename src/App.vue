c588685a678a3789fb1157505a31215cc9d4b1d4
Antonis <agiallelis1@gmail.com>
2021-03-28 16:23:29 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NavBar msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Carousel msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Carousel from './components/Carousel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Carousel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+    <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+        <template #header>BootstrapVue</template>
+
+        <template #lead>
+        This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+        featured content or information.
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+        It uses utility classes for typography and spacing to space content out within the larger
+        container.
+        </p>
+    </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
